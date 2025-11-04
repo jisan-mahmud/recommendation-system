c359e3d8ff2f21f52295afb6c32d6a0eda3cd898
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-11-04 13:17:35 +0600

    feat: customize product admin

--- product/admin.py
+++ product/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+    search_fields = ['name', 'category', 'description']
+    list_filter = ['category']
